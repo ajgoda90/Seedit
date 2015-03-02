e9be9d2d95aa6d814e296d4776fc03cf941dfc96
Andrew Hyte <hytead@mtc.byu.edu>
2015-03-02 09:49:03 -0700

    Got a pretty cool layout with nested views working

--- src/Frontend/JavaScript/mainApp.js
+++ src/Frontend/JavaScript/mainApp.js
@@ -1,4 +1,51 @@
 /**
  * Created by Andrew on 2/21/2015.
  */
-var app = angular.module("mainApp", ['dummyDAO']);
+var app = angular.module("mainApp", ['dummyDAO','ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider ){
+
+    $urlRouterProvider .otherwise('/home');
+
+    $stateProvider
+
+        .state('home',{
+            url: '/home',
+            templateUrl:'../HTML/test.html',
+            controller: 'mainController'
+        })
+
+            // nested list with custom controller
+            .state('home.list', {
+                url: '/list',
+                templateUrl: 'partial-home-list.html',
+                controller: function($scope) {
+                    $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+                }
+            })
+
+            // nested list with just some random string data
+            .state('home.paragraph', {
+                url: '/paragraph',
+                template: 'I could sure use a drink right now.'
+            })
+
+        .state('about', {
+            url: '/about',
+            views: {
+
+                // the main template will be placed here (relatively named)
+                '': { templateUrl: 'partial-about.html' },
+
+                // the child views will be defined here (absolutely named)
+                'columnOne@about': { template: 'Look I am a column!' },
+
+                // for column two, we'll define a separate controller
+                'columnTwo@about': {
+                    templateUrl: 'table-data.html',
+                    controller: 'scotchController'
+                }
+            }
+
+        });
+});
--- src/Frontend/JavaScript/mainController.js
+++ src/Frontend/JavaScript/mainController.js
@@ -1,3 +1,25 @@
 app.controller("mainController", function($scope) {
-    $scope.alertEM = function(message){alert(message);};
+    //$scope.alertEM = function(message){alert(message);};
+    console.log('It works!');
+});
+
+app.controller('scotchController', function($scope) {
+
+    $scope.message = 'test';
+
+    $scope.scotches = [
+        {
+            name: 'Macallan 12',
+            price: 50
+        },
+        {
+            name: 'Chivas Regal Royal Salute',
+            price: 10000
+        },
+        {
+            name: 'Glenfiddich 1937',
+            price: 20000
+        }
+    ];
+
 });
