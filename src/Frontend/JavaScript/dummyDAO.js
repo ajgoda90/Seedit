052a6688985f8b9b38fb1e271895e9c4b93e908c
Taylor Bell <taylorcgbell@gmail.com>
2015-03-14 18:15:17 -0600

    Adds new idea to api tags and seeds
    once a new idea is submitted it is added to the api tags and seeds.

--- src/Frontend/JavaScript/dummyDAO.js
+++ src/Frontend/JavaScript/dummyDAO.js
@@ -6,25 +6,36 @@
  */
 var dummyDAO = angular.module('dummyDAO',[]);
 dummyDAO.factory('api',function(){
+
+    var tags =  [
+        {"tagName":"cooking", "frequency":201, "hyperlink":"www.google.com"},
+        {"tagName":"video games", "frequency":100, "hyperlink":"www.google.com"},
+        {"tagName":"basketball","frequency":25, "hyperlink":"www.google.com"},
+        {"tagName":"baseball","frequency":23, "hyperlink":"www.google.com"},
+        {"tagName":"football","frequency":22, "hyperlink":"www.google.com"},
+        {"tagName":"hockey","frequency":21, "hyperlink":"www.google.com"},
+        {"tagName":"cricket","frequency":20, "hyperlink":"www.google.com"},
+        {"tagName":"swimming","frequency":19, "hyperlink":"www.google.com"}
+    ];
+
+    var seeds = [
+        {"seedTitle":"Chocolate Chip Cookies", "seedDescription":"A delicious chocolate chip cookie recipe!", "numberOfBranches":2001},
+        {"seedTitle":"BYU Fake Beard Card", "seedDescription":"Never shave again!", "numberOfBranches":1994},
+        {"seedTitle":"Best Magic deck", "seedDescription":"The best deck in Magic, the Gathering.  Branch only if expert.", "numberOfBranches":229}
+    ];
+
     var api = {};
     api.getTrendingTags = function(lowRange, highRange){
-        return [
-            {"tagName":"cooking", "frequency":201, "hyperlink":"www.google.com"},
-            {"tagName":"video games", "frequency":100, "hyperlink":"www.google.com"},
-            {"tagName":"basketball","frequency":25, "hyperlink":"www.google.com"},
-            {"tagName":"baseball","frequency":23, "hyperlink":"www.google.com"},
-            {"tagName":"football","frequency":22, "hyperlink":"www.google.com"},
-            {"tagName":"hockey","frequency":21, "hyperlink":"www.google.com"},
-            {"tagName":"cricket","frequency":20, "hyperlink":"www.google.com"},
-            {"tagName":"swimming","frequency":19, "hyperlink":"www.google.com"}
-        ]
+        return tags;
     }
     api.getTopSeeds = function(lowRange, highRange){
-        return [
-            {"seedTitle":"Chocolate Chip Cookies", "seedDescription":"A delicious chocolate chip cookie recipe!", "numberOfBranches":2001},
-            {"seedTitle":"BYU Fake Beard Card", "seedDescription":"Never shave again!", "numberOfBranches":1994},
-            {"seedTitle":"Best Magic deck", "seedDescription":"The best deck in Magic, the Gathering.  Branch only if expert.", "numberOfBranches":229}
-        ]
+        return seeds;
+    }
+    api.addTag = function(tag) {
+        tags.push(tag);
+    }
+    api.addSeed = function(seed) {
+        seeds.push(seed);
     }
     return api;
 });
